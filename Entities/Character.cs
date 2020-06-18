53f3b8dcc349309170c306999afe4172ade77fe4
ratto <rattopedro@gmail.com>
2020-06-17 23:41:49 -0300

    Character creation start

---
+++ Entities/Character.cs
@@ -0,0 +1,92 @@
+﻿using System.Text;
+
+/**
+* Character Class
+* This is the superclass for all the characters of the game.
+* Wether is a player character, a monster or a regular NPC that interacts 
+* with the player, will inherit this class characteristics.
+*/
+
+namespace eDaemon.Entities
+{
+    class Character
+    {
+        // Physical attributes
+        public int Strength { get; private set; }
+        public int Dexterity { get; private set; }
+        public int Agility { get; private set; }
+        public int Constitution { get; private set; }
+        // Mental attributes
+        public int Intelligence { get; private set; }
+        public int WillPower { get; private set; }
+        public int Perception { get; private set; }
+        public int Charisma { get; private set; }
+
+        private int _hpTotal;
+        private int _hpCurrent;
+
+        public Character()
+        {
+
+        }
+
+        public Character(int strength, int dexterity, int agility, int constitution, int intelligence, int willPower, int perception, int charisma)
+        {
+            Strength = strength;
+            Dexterity = dexterity;
+            Agility = agility;
+            Constitution = constitution;
+            Intelligence = intelligence;
+            WillPower = willPower;
+            Perception = perception;
+            Charisma = charisma;
+            _hpTotal = (Strength + Constitution) / 2; // Total HP equals the sum of Strength and Constitution scores divided by 2
+            _hpCurrent = _hpTotal;
+        }
+
+        // In Daemon RPG system the attribute modifier is 4 times the attribute score
+        public int getMod(int attribute)
+        {
+            return attribute * 4;
+        }
+
+        // HP control
+        public int getTotalHP()
+        {
+            return _hpTotal;
+        }
+
+        public int getCurrentHP()
+        {
+            return _hpCurrent;
+        }
+
+        public void increaseHP(int hp)
+        {
+            _hpCurrent += hp;
+        }
+
+        public void decreaseHP(int hp)
+        {
+            _hpCurrent -= hp;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("Força " + Strength + " / " + getMod(Strength));
+            sb.Append("Destreza " + Dexterity + " / " + getMod(Dexterity));
+            sb.Append("Agilidade " + Agility + " / " + getMod(Agility));
+            sb.Append("Constituição " + Constitution + " / " + getMod(Constitution));
+
+            sb.Append("Inteligência " + Intelligence + " / " + getMod(Intelligence));
+            sb.Append("Força de Vontade " + WillPower + " / " + getMod(WillPower));
+            sb.Append("Percepção " + Perception + " / " + getMod(Perception));
+            sb.Append("Carisma " + Charisma + " / " + getMod(Charisma));
+
+            sb.Append("HP " + _hpTotal + " / " + _hpCurrent);
+
+            return sb.ToString();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using eDaemon.Entities;
 
 namespace eDaemon
 {
@@ -6,7 +7,31 @@ namespace eDaemon
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Character playerCharacter;
+
+            Console.WriteLine("Criação de personagem para Daemon");
+            Console.WriteLine("Escolha uma classe: ");
+            Console.WriteLine("1 - Tanque (FR 15, DEX 14, AGI 14, CONS 18, INT 10, WILL 10, PER 9, CAR 11)");
+            Console.WriteLine("2 - Arqueiro (FR 12, DEX 17, AGI 16, CONS 11, INT 10, WILL 12, PER 14, CAR 9)");
+            int classChoice = int.Parse(Console.ReadLine());
+
+            switch (classChoice)
+            {
+                case 1:
+                    playerCharacter = new Character(15, 14, 14, 18, 10, 10, 9, 11);
+                    break;
+                case 2:
+                    playerCharacter = new Character(12, 17, 16, 11, 10, 12, 14, 9);
+                    break;
+                default:
+                    Console.WriteLine("escolha inválida.");
+                    break;
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine("Personagem escolhido: ");
+            
         }
     }
 }
