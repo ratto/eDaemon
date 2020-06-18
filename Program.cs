0f72be3083ab205dd188b4b6dd2db44f3d178693
ratto <rattopedro@gmail.com>
2020-06-17 22:27:25 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace eDaemon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
